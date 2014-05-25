e742ea949316d7e59cae55feb6d60309529d8af7
poc7667 <poc7667@gmail.com>
2014-05-25 22:48:32 +0800

    Fix: precompile problem , should add precompile path into
    ActionView::Template::Error (Asset filtered out and will not be served: add `Rails.application.config.assets.precompile += %w( economizer/images/logo.png )` to `config/initializers/assets.rb` and restart your server):
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,12 @@ module Lesson4am
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.assets.paths << "#{Rails.root}/vendor/themes"
+    config.assets.precompile += %w( vendor/themes/**/application.js )
+    config.assets.precompile += %w( vendor/themes/**/application.css )
+    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+
+    # Precompile *all* assets, except those that start with underscore
+    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/    
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
