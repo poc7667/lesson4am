4c195d9cd72f987697cb82e5a3a1f5ff0b0ac2f7
poc7667 <poc7667@gmail.com>
2014-06-02 12:37:27 +0800

    Add column:"add_accumulated_percentage_to_tw_labor_incomes"
    rails g migration add_accumulated_percentage_to_tw_labor_incomes accumulated_percentage:float
    
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602031731) do
+ActiveRecord::Schema.define(version: 20140602043616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140602031731) do
     t.float    "income"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "accumulated_percentage"
   end
 
   create_table "users", force: true do |t|
