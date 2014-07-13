6d26a2c27d7101c84b3ae2d21243b284a386c7d5
poc7667 <poc7667@gmail.com>
2014-07-13 16:50:48 +0800

    Add loan_duration, house_price columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712071518) do
+ActiveRecord::Schema.define(version: 20140713065054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20140712071518) do
     t.hstore   "expenditure"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "house_price"
+    t.integer  "loan_duration"
   end
 
   create_table "identities", force: true do |t|
