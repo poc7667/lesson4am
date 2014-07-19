a0d3ef851ae497dc404ed2e9472fe35b42431acd
poc7667 <poc7667@gmail.com>
2014-07-19 11:47:55 +0800

    Add mail gun smtp setting
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,18 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Add for mailgun smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => ENV['mailgun_domain'],
+    :user_name => "postmaster@#{ENV['mailgun_domain']}",
+    :password => ENV['mailgun_passwd']
+  }
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
