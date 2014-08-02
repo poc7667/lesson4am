4c020e6192992825e9b5c087512d7a1a341895ba
poc7667 <poc7667@gmail.com>
2014-08-02 21:50:59 +0800

    Validate update file
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- app/models/joseph_memo.rb
+++ app/models/joseph_memo.rb
@@ -1,4 +1,7 @@
 class JosephMemo < ActiveRecord::Base
+  has_attached_file :imgs
+  # do_not_validate_attachment_file_type :imgs
+  validates_attachment :imgs, content_type: { content_type: ["application/zip"] }
   validates :name, presence: true
   validates :title, presence: true
   validates :words, presence: true
