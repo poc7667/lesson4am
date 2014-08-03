d6f89770f4168f178bfd2383fe5b74ebf3c04b12
poc7667 <poc7667@gmail.com>
2014-08-03 10:59:03 +0800

    Add correct timezone
    Signed-off-by: poc7667 <poc7667@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Lesson4am
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Taipei'
     config.encoding = "utf-8"
     config.assets.paths << "#{Rails.root}/vendor/themes"
     config.assets.precompile += %w( vendor/themes/**/application.js )
