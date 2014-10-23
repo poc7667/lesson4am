778114e5fc79ebe33917fa2ca3f298d17eb5b625
poc7667 <poc7667@gmail.com>
2014-10-23 13:28:45 +0800

    Add unique index '"publish_date", "prizes"'

--- db/migrate/20141023052602_create_invoices.rb
+++ db/migrate/20141023052602_create_invoices.rb
@@ -3,8 +3,8 @@ class CreateInvoices < ActiveRecord::Migration
     create_table :invoices do |t|
       t.datetime :publish_date
       t.text :prizes
-
       t.timestamps
+      add_index ["publish_date", "prizes"], :unique => true
     end
   end
 end
